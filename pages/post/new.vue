7e4eb1b83e63d8c8378cbe5e613913e2c7f6681a
Naoko3in4 <okashimochi@gimail.com>
2019-07-29 21:03:18 +0900

    fixed css

--- pages/post/new.vue
+++ pages/post/new.vue
@@ -1,38 +1,46 @@
 <template>
   <div class="container">
     <p>新規登録</p>
-    <div class="input_container">
-      <div class="input_box">
+    <div class="input__container">
+      <div class="input__box">
         <p>タイトル</p>
         <p>
           <input
-          v-model="formData.inputTitle"
-          type="text"
+            class=input_item
+            v-model="formData.inputTitle"
+            type="text"
           >
         </p>
       </div>
-      <div class="input_box">
+      <div class="input__box">
         <p>ユーザー名</p>
         <p>
           <input
-          v-model="formData.inputUserName"
-          type="text"
+            class=input_item
+            v-model="formData.inputUserName"
+            type="text"
           >
         </p>
       </div>
-      <div class="input_box">
+      <div class="input__box">
         <p>内容</p>
-        <textarea v-model="formData.inputText" />
+        <textarea
+          class="textarea_item"
+          v-model="formData.inputText" />
       </div>
     </div>
-    <div class="submit_button_container">
-      <button class="submit_button" @click="addMemo()">送信</button>
+    <div class="submit_button__container">
+      <button class="submit_button"
+        @click="addMemo()">
+        送信
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  layout: 'body',
   data: () => ({
     formData: {
       inputText: "",
@@ -53,45 +61,45 @@ export default {
 </script>
 
 <style>
-body {
-  width: 100vw;
-  margin: 0 auto;
-  font-size: 62.5%;
-}
 .container {
-  font-size: 1.2rem;
   width: 60vw;
   margin: 0 auto;
   padding: 1rem 0;
 }
 
-.input_container {
+.container,
+.input_item,
+.textarea_item {
+  font-size: 1.2rem;
+}
+
+.input__container {
   width: 100%;
   margin-bottom: 2rem;
 }
 
-.input_box {
+.input__box {
   width: 100%;
 }
 
-input {
+.input_item {
   width: 100%;
   appearance: none;
-  font-size: 1.2rem;
   border-radius: 5px;
   padding: 0.5rem;
 }
 
-textarea {
+.textarea_item {
   border: 1px solid gray;
   width: 100%;
   height: 10rem;
 }
 
-.submit_button_container {
+.submit_button__container {
   display: flex;
   justify-content: flex-end;
 }
+
 .submit_button {
   background-color: #fff;
   padding: 0.5rem 2rem;
