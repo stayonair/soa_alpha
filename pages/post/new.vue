21fabc7c33c2c7f18bd1016f8977a525e4795814
Ryuji <ryujineer@gmail.com>
2019-07-29 23:09:45 +0900

    add method to send data to firebase

--- pages/post/new.vue
+++ pages/post/new.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-    <p></p>
+    <button @click="addPosts()">追加</button>
   </div>
 </template>
 
 <script>
-export default {
+import { mapActions } from 'vuex'
 
+export default {
+  data: () => ({
+    name: 'Ryuji'
+  }),
+
+  created() {
+    this.initPosts()
+  },
+
+  methods: {
+    ...mapActions('post', [
+      'initPosts',
+      'addPostsToFB',
+    ]),
+    addPosts() {
+      const addPostData = {
+        name: this.name,
+        date: new Date()
+      }
+      this.addPostsToFB(addPostData)
+    }
+  }
 }
 </script>
 
