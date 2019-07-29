7e63d7549f9392e187b945a6a6f0a161d2018175
Shohei_Japan <kai.entertainment0304@gmail.com>
2019-07-29 21:52:33 +0900

    add recording method on page new

--- pages/post/new.vue
+++ pages/post/new.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
     <p>new</p>
+    <button
+      type="button" 
+      @click="start()" 
+    >
+      start!!
+    </button>
+    <button @click="stop()">
+      stop!!
+    </button>
+    <audio v-if="audioData" controls :src="audioData" />
   </div>
 </template>
 
 <script>
-export default {
+import record from '~/utils/record'
 
+export default {
+  data: () => ({
+    audioData: null
+  }),
+  methods: {
+    start() {
+      record.recStart()
+    },
+    async stop() {
+      const res = await record.stopRecording()
+      const url = URL.createObjectURL(res)
+      this.audioData = url
+    }
+  }
 }
 </script>
 
