a42a79bd2d1ab0450d23395afec5abbdce075821
Ryuji <ryujineer@gmail.com>
2019-08-07 00:16:37 +0900

    get detail post page from firebase

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -1,12 +1,57 @@
 <template>
   <div>
-    <p>{{ $route.params }}</p>
+    <p>ポストID: {{ postId }}</p>
+    <p>タイトル: {{ postTitle }}</p>
+    <p>ユーザー: {{ userName }}</p>
+    <p>内容: {{ postText }}</p>
+    <audio controls :src="audioUrl"></audio>
   </div>
 </template>
 
 <script>
-export default {
+import firebase from '~/plugins/firebase'
+// import { mapActions, mapGetters } from 'vuex'
 
+const db = firebase.firestore()
+const postsCollection = db.collection('posts')
+
+export default {
+  data: () => ({
+    postId: '',
+    postTitle: '',
+    userName: '',
+    postText: '',
+    audioUrl: '',
+  }),
+  created() {
+    // URLパラメータを取得
+    const id = this.$nuxt.$route.params.id
+
+    // this.getPost(id)
+
+    postsCollection.doc(id).get().then(doc => {
+      const data = doc.data()
+      this.postId = data.postId
+      this.postTitle = data.postTitle
+      this.userName = data.userName
+      this.postText = data.postText
+      this.audioUrl = data.audioUrl
+    })
+  },
+
+  // 一応 vuexfire の関数も残しておきます
+  // ↓↓↓↓↓
+  // computed: {
+  //   ...mapGetters('post', [
+  //     'post'
+  //   ])
+  // },
+  // methods: {
+  //   ...mapActions('post', [
+  //     'getPost'
+  //   ]),
+  // }
+  
 }
 </script>
 
--- store/post.js
+++ store/post.js
@@ -6,13 +6,17 @@ const db = firebase.firestore()
 const postsCollection = db.collection('posts')
 
 export const state = () => ({
-  posts: []
+  posts: [],
+  // post: []
 })
 
 export const getters = {
   posts: state => {
     return state.posts
-  }
+  },
+  // post: state => {
+  //   return state.post
+  // }
 }
 
 export const actions = {
@@ -20,6 +24,16 @@ export const actions = {
   initPosts: firestoreAction(({ bindFirestoreRef }) => {
     bindFirestoreRef('posts', postsCollection)
   }),
+
+  // 第2引数にドキュメント名を渡せば、コレクション以降にもアクセス可能
+  // 拡張性ありだから上の関数書き直しても良いかも
+  // getPost: firestoreAction(({ bindFirestoreRef } , doc) => {
+  //   if (!doc) {
+  //     bindFirestoreRef('post', postsCollection)
+  //     return
+  //   }
+  //   bindFirestoreRef('post', postsCollection.doc(doc))
+  // }),
 
    addPostToFB: firestoreAction(async (context, data) => {
     await postsCollection.doc(data.postId).set(data)
